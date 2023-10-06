d2ea851b10009e6949766e3989a70866ba08bea5
Connor Robertson <cnr.robertson@gmail.com>
2023-10-06 14:32:19 -0700

    add preview to telescope terminal picker

--- lua/nuiterm/telescope.lua
+++ lua/nuiterm/telescope.lua
@@ -1,6 +1,7 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local actions = require("telescope.actions")
+local previewers = require("telescope.previewers")
 local action_state = require("telescope.actions.state")
 local conf = require("telescope.config").values
 local menu = require("nuiterm.menu")
@@ -24,12 +25,38 @@ terminal_finder.picker = function(opts)
 
 		return finders.new_table({ results = terminals, entry_maker = nuiterm_maker })
 	end
+  local nuiterm_previewer = previewers.new_buffer_previewer({
+    title = "Terminal contents",
+    define_preview = function(self, entry, _)
+      local height = vim.api.nvim_win_get_height(self.state.winid)
+      local term_bufnr = Nuiterm.terminals[entry.value.type][entry.value.type_id].bufnr
+      local term_lines = vim.api.nvim_buf_get_lines(term_bufnr, -2*height, -1, false)
+      -- Remove any repeating empty rows
+      local new_term_lines = {}
+      local was_empty = false
+      for _,l in pairs(term_lines) do
+        if l == "" then
+          local is_empty = true
+          if is_empty and not was_empty then
+            table.insert(new_term_lines, l)
+          end
+          was_empty = true
+        else
+          table.insert(new_term_lines, l)
+          was_empty = false
+        end
+      end
+      new_term_lines = {unpack(new_term_lines, math.max(#new_term_lines-height+1,1))}
+      vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, new_term_lines)
+    end
+  })
 
 	pickers.new(opts, {
 		prompt_title = "Select a terminal",
-		results_title = "nuiterms",
+		results_title = "Nuiterms",
 		finder = nuiterm_finder(),
 		sorter = conf.generic_sorter(opts),
+    previewer = nuiterm_previewer,
 
 		attach_mappings = function(prompt_bufnr, map)
 			actions.select_default:replace(function()
