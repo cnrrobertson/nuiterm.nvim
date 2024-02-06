ba219dfd45e3027aedee586e35875340027d97d8
Connor Robertson <cnr.robertson@gmail.com>
2024-02-05 17:08:53 -0800

    update: fix active terminal test

--- tests/test_toggle.lua
+++ tests/test_toggle.lua
@@ -36,7 +36,7 @@ T['toggle_default'] = function()
   nequals(string.find(child.api.nvim_buf_get_name(0), "buffer"), nil)
 
   -- Buffer has active terminal
-  equals(child.api.nvim_get_mode().mode, "nt")
+  equals(child.api.nvim_get_mode().mode, "t")
 
   -- Buffer number correct
   local term_name = child.api.nvim_buf_get_name(0)
